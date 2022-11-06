0096aa0e1b755d18f2725f07ed8945f38c7455a8
Huáscar Miranda <huascarm@gmail.com>
2022-11-06 19:41:31 -0400

    deploy to moonbase

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,14 +5,25 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  // networks: {
+  //   mumbai: {
+  //     url: process.env.TESTNET_RPC,
+  //     accounts: [process.env.PRIVATE_KEY || "default"],
+  //   },
+  // },
+  // etherscan: {
+  //   apiKey: process.env.POLYGONSCAN_API_KEY,
+  // },
   networks: {
-    mumbai: {
+    moonbeam: {
       url: process.env.TESTNET_RPC,
+      chainId: 1287,
       accounts: [process.env.PRIVATE_KEY || "default"],
     },
   },
   etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
+    // apiKey: process.env.POLYGONSCAN_API_KEY,
+    apiKey: process.env.MOONBASESCAN_API_KEY,
   },
 };
 
