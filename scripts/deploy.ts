d1e8f12fa8dfbc631ae6799a589da3904f60b853
Huáscar Miranda <huascarm@gmail.com>
2022-11-10 18:08:16 -0400

    Fixing miss file founding

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,14 +3,11 @@ import { ethers } from "hardhat";
 async function main() {
   const Bonvo = await ethers.getContractFactory("Bonvo");
   const bonvo = await Bonvo.deploy();
-
   await bonvo.deployed();
 
   console.log(`Bonvo was deployed to ${bonvo.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
